e01e8d69f7d9303a56f0ad554ca68d6cc53007ee
ZenMaster02 <zenen2002@gmail.com>
2023-07-16 13:15:36 -0400

    Initialization

---
+++ SkipListSet.java
@@ -0,0 +1,158 @@
+package SkipList_Project.SkipList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.SortedSet;
+
+public class SkipListSet<T extends Comparable<T>> implements SortedSet<T> {
+    int size = 0;
+    Iterator<T> iterator;
+    private class SkipListSetIterator<T extends Comparable<T>> implements Iterator<T> {
+
+        @Override
+        public boolean hasNext() {
+    
+            return false;
+        }
+    
+        @Override
+        public T next() {
+    
+            return null;
+        }
+
+        @Override
+        public void remove() {
+
+        }        
+    }
+    
+    private class SkipListSetItem<T extends Comparable<T>>
+    {
+        T payload;
+        SkipListSetItem next;
+
+    }
+    // Sorted set functions
+    @Override
+    public T first() {
+
+        return null;
+    }
+
+    @Override
+    public T last() {
+
+        return null;
+    }
+
+    @Override
+    public SortedSet<T> subSet(T fromElement, T toElement) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SortedSet<T> headSet(T toElement) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SortedSet<T> tailSet(T fromElement) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Comparator<? super T> comparator() {
+        return null;
+    }
+
+    // Set functions
+    @Override
+    public boolean add(T e) {
+
+        return false;
+    }
+    
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+    
+    @Override
+    public void clear() {
+
+    }
+    
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object o : c)
+        {
+            boolean res = contains(o);
+            if (!res)
+            {
+                return false;
+            }            
+        }
+        return true;
+    }
+
+    public boolean equals (Object o)
+    {
+        return false;
+    }
+
+    public int hashCode()
+    {
+        return 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (size() == 0);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return iterator;
+    }
+    
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Object[] toArray() {
+
+        return null;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T[] toArray(T[] a) {
+
+        return null;
+    }   
+}
